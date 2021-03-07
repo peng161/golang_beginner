2fe559568e028d0e5022accfbd0b4e062818441d
pxp <puanxuepeng@gmail.com>
2021-03-07 22:56:49 +0800

    include int arr for bubble sort

--- sort/bubble_sort.go
+++ sort/bubble_sort.go
@@ -3,18 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+
 	floatArr := []float64{32.1, 10.5, 36.8, 21.4, 62.1, 50.4}
-	loopThru(floatArr)
+	intArr := []int{32, 48, 12, 31, 25, 28}
+	fmt.Println("Float Arr")
+	loopThruFloat64(floatArr)
+	fmt.Println("Int Arr")
+	loopThruInt(intArr)
 }
 
-func loopThru(floatArr []float64) {
+func loopThruFloat64(floatArr []float64) {
 	for i := 0; i < len(floatArr); i++ {
-		pushLargerValue(floatArr, i)
+		pushLargerValueFloat64(floatArr, i)
 	}
 }
 
-func pushLargerValue(floatArr []float64, index int) {
+func pushLargerValueFloat64(floatArr []float64, index int) {
 
 	for i := 0; i < len(floatArr)-1-index; i++ {
 		if floatArr[i] > floatArr[i+1] {
@@ -23,4 +27,21 @@ func pushLargerValue(floatArr []float64, index int) {
 		}
 	}
 	fmt.Printf("No %v Largest Value: %v\n", index+1, floatArr[len(floatArr)-(1+index)])
+}
+
+func loopThruInt(intArr []int) {
+	for i := 0; i < len(intArr); i++ {
+		pushLargerValue(intArr, i)
+	}
+}
+
+func pushLargerValue(intArr []int, index int) {
+
+	for i := 0; i < len(intArr)-1-index; i++ {
+		if intArr[i] > intArr[i+1] {
+			//fmt.Printf("Larger Value: %v, %v\n", floatArr[i], len(floatArr))
+			intArr[i], intArr[i+1] = intArr[i+1], intArr[i]
+		}
+	}
+	fmt.Printf("No %v Largest Value: %v\n", index+1, intArr[len(intArr)-(1+index)])
 }
